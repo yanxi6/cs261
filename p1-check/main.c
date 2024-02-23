935c3f94f54eda27e97eedd00707f306e4738b41
yanxi6 <yanxi48@gmail.com>
2024-02-24 02:04:06 +0800

    experiments on p1-check

--- p1-check/main.c
+++ p1-check/main.c
@@ -17,7 +17,18 @@ int main (int argc, char **argv)
 {
     // Write code to call the parse function then 
     // open the elf file, read its header, and react depending on whether -H was present
-    
+    char *fname = "tests/inputs/simple.o";
+    FILE* fp = fopen(fname, "r");
+    // elf_hdr_t *hdr;
+    char data[8];
+    fread(data, 8, 1, fp);
+    for(int i = 0; i < 8; i++)
+    {
+        printf("%x ", data[i]);
+    }
+    printf("\n");
+    // read_header(fname);
+    fclose(fp);
     return EXIT_SUCCESS;
 }
 
