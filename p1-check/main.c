6e176d4e8dc65494817540f7bd10a584e462a357
yanxi6 <yanxi48@gmail.com>
2024-02-24 23:28:48 +0800

    finish parsing command line & read header & ready for dumping header info

--- p1-check/main.c
+++ p1-check/main.c
@@ -17,13 +17,32 @@ int main (int argc, char **argv)
 {
     // Write code to call the parse function then 
     // open the elf file, read its header, and react depending on whether -H was present
-    char *fname = "tests/inputs/simple.o";
-    char *argv_v[] = { "./y86", "-H", "empty.o", NULL };
-    bool rc = 0;
-    bool header = false;
-    char *file = NULL;
-
-    rc = parse_command_line_p1 (3, argv_v, &header, &file);
+    char* fname;
+    bool header_dumped;
+    elf_hdr_t header;
+    if (!parse_command_line_p1(argc, argv, &header_dumped, &fname))
+    {
+        exit(EXIT_FAILURE);
+    }
+    if(!fname)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    // char *wd =getcwd(NULL, 0);
+    // FILE* fp = fopen(fname, "r");
+    FILE* fp = fopen(fname, "r");
+    if(!fp)
+    {
+        puts("Failed to read file");
+		exit(EXIT_FAILURE);
+    }
+    if(!read_header(fp, &header))
+    {
+        puts("Failed to read file");
+		exit(EXIT_FAILURE);
+    }
+    dump_header(header);
+    fclose (fp);
     return EXIT_SUCCESS;
 }
 
--- p1-check/p1-check.c
+++ p1-check/p1-check.c
@@ -23,8 +23,10 @@ bool parse_command_line_p1 (int argc, char **argv, bool *header, char **file)
 {
     // Implement this function
     // Read system argv and parsing
-    int opt;
-    while ((opt = getopt(argc, argv, "+hH")) != -1) 
+    // h: print help message
+    // H: check elf header
+    int opt = 0;
+    while ((opt = getopt(argc, argv, "hH")) != -1) 
     {
         switch (opt)
         {
@@ -34,17 +36,24 @@ bool parse_command_line_p1 (int argc, char **argv, bool *header, char **file)
                 *file = NULL;
                 return true;
             case 'H':
+                if (argc == 2)
+                {
+                    // handle missing filename
+                    usage_p1();
+                    return false;
+                }
                 *header = true;
                 *file = NULL;
                 break;
             default:
-            // invalid options
+                // invalid options
                 usage_p1();
                 *header = false;
                 *file = NULL;
                 return false;
         }
     }
+
     *file = argv[optind];
     return true;
 }
@@ -52,12 +61,34 @@ bool parse_command_line_p1 (int argc, char **argv, bool *header, char **file)
 bool read_header (FILE *file, elf_hdr_t *hdr)
 {
     // Implement this function
-    // fread(data, 8, 1, fp);
-    return false;
+    if(!file || !hdr)
+    {
+        return false;
+    }
+    if(fread(hdr, sizeof(elf_hdr_t), 1, file) == -1)
+    {
+        return false;
+    }
+    if (hdr->magic != ntohl(0x454C4600))
+	{
+		//bad magic number
+		return false;
+	}
+    return true;
 }
 
 void dump_header (elf_hdr_t hdr)
 {
     // Implement this function
+    // printf("%x ", hdr.e_version);
+    // printf("%x ", hdr.e_entry);
+    // printf("%x ", hdr.e_phdr_start);
+    // printf("%x ", hdr.e_num_phdr);
+    // printf("%x ", hdr.e_symtab);
+    // printf("%x ", hdr.e_strtab);
+    // printf("%x ", hdr.magic);
+    // printf("Mini-ELF version 1");
+    // printf("Entry point 0x100");
+
 }
 
